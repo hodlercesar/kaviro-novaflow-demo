53fa345661e693fac9850d88582abdc4d3b446f0
Hodler César <empresakavirostudio@gmail.com>
2026-08-26 16:17:52 -0400

    fix: place ClerkProvider inside app body

--- app/layout.js
+++ app/layout.js
@@ -8,10 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
+    </html>
   );
 }
